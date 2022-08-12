4b51c72094d7d7537c57451d0d5502b64330b655
Nathaniel Campbell <nathanielcampbell2002@gmail.com>
2022-08-12 12:29:14 -0500

    Minor changes

--- src/app/shopping/shopping-list/list.component.ts
+++ src/app/shopping/shopping-list/list.component.ts
@@ -24,7 +24,7 @@ export class ListComponent implements OnInit {
     }
 
     updateQuantity(item: Item, amount: -1 | 1): void {
-        const newQuantity = amount === -1 ? Math.max(item.quantity! + amount, 1) : Math.min(item.quantity! + amount, 10);
+        const newQuantity = amount === -1 ? Math.max(item.quantity! + amount, 1) : Math.min(item.quantity! + amount, 999);
 
         this.listService.updateItem(item._id!, { ...item, quantity: newQuantity }).subscribe((res) => {
             if (res.status === 'success') this._getAllItems();
